9648cb6165b1a253900325500d38bd0a8b0ab22b
pranotobudi <pranotobudi.s@gmail.com>
2018-12-24 19:13:18 +0700

    created the subscriber URL

--- CRM_apps/urls.py
+++ CRM_apps/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from marketing.views import HomePage
-
+from subscribers.views import subscriber_new
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', HomePage.as_view(), name="home"),
+    path('signup/',subscriber_new, name='sub_new'),
 
 ]
